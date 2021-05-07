861e2e0546e19fb95fe8a49f6cdb5e3c1e8fed85
nachomarin97 <nachomarin97@gmail.com>
2021-05-07 15:24:52 -0600

    Se añadió el REST API simple y el cliente para consumir dicho servicio

---
+++ RESTGreeterClient/RESTGreeterClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace RESTGreeterClient
+{
+    class Program
+    {
+        private static readonly HttpClient Client = new HttpClient();
+
+        private static string APIUrl = "https://localhost:44305/greeter";
+
+        static async Task Main(string[] args)
+        {
+            System.Net.ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+
+            Client.BaseAddress = new Uri(APIUrl);
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/string"));
+
+            var watch = new Stopwatch();
+            watch.Start();
+
+            var replyTask = Client.GetStringAsync(APIUrl);
+            var reply = await replyTask;
+
+            watch.Stop();
+            var responseTime = watch.ElapsedMilliseconds;
+            Console.WriteLine("Greeting: " + reply);
+            Console.WriteLine("The reply lasted: " + responseTime + " ms");
+            Console.WriteLine("Press any key to exit...");
+        }
+    }
+}
